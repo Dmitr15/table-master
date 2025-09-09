73d425b608efe237e1f77c2510c502893aac6c26
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-09-09 17:29:56 +0300

    Removed some extra methods

--- app/Http/Controllers/ProcessFileController.php
+++ app/Http/Controllers/ProcessFileController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Illuminate\Support\Facades\Storage;
 
 class ProcessFileController extends Controller
 {
-    //
+    public function viewFile(string $filePath):array
+    {
+        if (!file_exists($filePath)) {
+            throw new \Exception("File not found: " . $filePath);
+        }
+     
+        $spreadsheet = IOFactory::load($filePath);
+        
+        $sheet = $spreadsheet->getActiveSheet();
+        
+        $data = $sheet->toArray();
+
+        return $data;
+    }
+    
 }
