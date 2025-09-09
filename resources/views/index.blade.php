5ee7c92500de6cdd2f97f8cfebb19ca79f3a41a4
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-09-09 17:31:48 +0300

    Some minor changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Table Master</title>
 </head>
 <body>
+    <form action="{{route('process.form')}}" method="POST" enctype="multipart/form-data">
+    @csrf
     
+    <div>
+        <label for="xls_file">Choose file</label>
+        <input type="file" name="xls_file" id="xls_file">
+    </div>
+    </form>
 </body>
 </html>
