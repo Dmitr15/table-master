8bd904f05857ffd3ecc4f55c3809f1e0e752791a
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-09-10 13:29:27 +0300

    Added fields to the table

--- app/Models/UserFile.php
+++ app/Models/UserFile.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class UserFile extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'original_name',
+        'path'
+    ];
+
+    public function file(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
