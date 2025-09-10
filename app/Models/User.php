62a69fec4a1344d7ffd989eb11a26ade9e7d72bd
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-09-10 13:28:36 +0300

    Added relationship between tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Authenticatable
+class User extends Authenticatable 
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +46,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function excelFiles(): HasMany
+    {
+        return $this->hasMany(UserFile::class);
     }
 }
