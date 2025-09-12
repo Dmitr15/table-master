86cadc21b0a945a71ceea294667e8f65551dbd1c
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-09-12 21:51:39 +0300

    Route dashboard was modified

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index')->name('index');
 
+Route::get('/dashboard', [FileController::class, 'index'])->name('dashboard');
+
 Route::resource('files', FileController::class);
 
 Route::post('/process', [ProcessFileController::class, 'viewFile'])->name('process.form');
